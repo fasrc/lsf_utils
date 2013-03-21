9bd6c0dfc02788c44b1350fef6e360db39ae61c5
John Brunelle <john_brunelle@harvard.edu>
2013-03-21 18:08:16 -0400

    print hint after make test

--- d_lfs_migrate/d_lfs_migrate.c
+++ d_lfs_migrate/d_lfs_migrate.c
@@ -24,6 +24,8 @@ John Brunelle
 #define LOV_EA_SIZE(lum, num) (sizeof(*lum) + num * sizeof(*lum->lmm_objects))
 #define LOV_EA_MAX(lum) LOV_EA_SIZE(lum, MAX_OSTS)
 
+char find_only = 1;
+
 static int lfs_migrate(const char *fpath, const struct stat *sb, int tflag) {
 	struct lov_user_md_v1 *lum;  //lustre stripe info
 	int ostidx;                  //OST number
@@ -33,7 +35,7 @@ static int lfs_migrate(const char *fpath, const struct stat *sb, int tflag) {
 	int status = 0;  //child status
 	int r = 0;       //function return value
 	
-	char *args[] = {"lfs_migrate", "-y", (char*)NULL, (char*)NULL};
+	char *args[] = {"lfs_migrate", "-n", (char*)NULL, (char*)NULL};
 	char *fpathc = NULL;
 
 	switch (tflag) {
@@ -57,46 +59,46 @@ static int lfs_migrate(const char *fpath, const struct stat *sb, int tflag) {
 					ostidx = lum->lmm_objects[0].l_ost_idx;
 					//fprintf(stdout, "got a file: %s: obdidx: %d\n", fpath, ostidx);
 				
-					/*
-					//(it would be better to do this only in the child, but fork() support in mpi on openib is very fragile, so keeping as much as possible in the parent)
-					fpathc = (char *)malloc((strlen(fpath)+1) * sizeof(*fpathc));
-					if (fpathc==NULL) { fprintf(stderr, "*** ERROR *** malloc failed\n"); exit(1); }
-					strcpy(fpathc, fpath);
-					args[2] = fpathc;
-					*/
+					if (!find_only) {
+						//(it would be better to do this only in the child, but fork() support in mpi on openib is very fragile, so keeping as much as possible in the parent)
+						fpathc = (char *)malloc((strlen(fpath)+1) * sizeof(*fpathc));
+						if (fpathc==NULL) { fprintf(stderr, "*** ERROR *** malloc failed\n"); exit(1); }
+						strcpy(fpathc, fpath);
+						args[2] = fpathc;
+					}
 					
-					if (ostidx<=5) {  //panoss01 == panlfs-OST000{0..5}
-						fprintf(stdout, "found a file on panoss01, obdidx %d: %s\n", ostidx, fpath);
-
-						/*
-						pid = fork();
-						if (pid==-1) {
-							fprintf(stderr, "*** ERROR *** (parent) failed to fork, errno: %d\n", errno);
-							return errno;
-						}
-						if (pid==0) {
-							execvp(args[0], args);
-							//(control only gets here if exec fails)
-							_exit(errno);  //(note that errno will be ENOENT (2) if binary of library is not found, not 127 like shell uses)
-						}
-						else {
-							waitpid(pid, &status, 0);
-							if (status!=0) {
-								if (WIFSIGNALED(status)) {
-									fprintf(stderr, "*** ERROR *** lfs_migrate for [%s] terminated by signal: %d\n" , fpath, WTERMSIG(status));
-								} else if (WIFEXITED(status)) {
-									fprintf(stderr, "*** ERROR *** lfs_migrate for [%s] exited with status: %d\n"   , fpath, WEXITSTATUS(status));
-								} else {
-									fprintf(stderr, "*** ERROR *** lfs_migrate for [%s] ended with raw status: %d\n", fpath, status);
+					if (ostidx==6) {
+						if (find_only) {
+							fprintf(stdout, "found a file on to migrate, on obdidx %d: %s\n", ostidx, fpath);
+						} else {
+							pid = fork();
+							if (pid==-1) {
+								fprintf(stderr, "*** ERROR *** (parent) failed to fork, errno: %d\n", errno);
+								return errno;
+							}
+							if (pid==0) {
+								execvp(args[0], args);
+								//(control only gets here if exec fails)
+								_exit(errno);  //(note that errno will be ENOENT (2) if binary of library is not found, not 127 like shell uses)
+							}
+							else {
+								waitpid(pid, &status, 0);
+								if (status!=0) {
+									if (WIFSIGNALED(status)) {
+										fprintf(stderr, "*** ERROR *** lfs_migrate for [%s] terminated by signal: %d\n" , fpath, WTERMSIG(status));
+									} else if (WIFEXITED(status)) {
+										fprintf(stderr, "*** ERROR *** lfs_migrate for [%s] exited with status: %d\n"   , fpath, WEXITSTATUS(status));
+									} else {
+										fprintf(stderr, "*** ERROR *** lfs_migrate for [%s] ended with raw status: %d\n", fpath, status);
+									}
 								}
 							}
 						}
-						*/
 					}
 
-					/*
-					free(fpathc); fpathc = NULL;
-					*/
+					if (!find_only) {
+						free(fpathc); fpathc = NULL;
+					}
 				}
 			}
 			free(lum); lum = NULL;
